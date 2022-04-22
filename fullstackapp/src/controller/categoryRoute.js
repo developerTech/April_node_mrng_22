b68cd0943eaf7a2169310856b7396faaf5e82db9
developerfunnel@gmail.com <ahanda205@gmail.com>
2022-04-22 08:55:37 +0530

    mongodb

--- fullstackapp/src/controller/categoryRoute.js
+++ fullstackapp/src/controller/categoryRoute.js
@@ -1,28 +1,7 @@
 let express = require('express');
 let categoryRouter = express.Router();
-
-var category = [
-    {
-        "id":1,
-        "category": "Fashion",
-        "thumb":"https://i.ibb.co/56VP0Fn/cloths.jpg"
-    },
-    {
-        "id":2,
-        "category":"Electronics",
-        "thumb":"https://i.ibb.co/pw5Wtdx/appliances.jpg"
-    },
-    {
-        "id":3,
-        "category":"Essentials",
-        "thumb":"https://i.ibb.co/0cw34xm/essentials.jpg"
-    },
-    {
-        "id":4,
-        "category": "Footwear",
-        "thumb":"https://i.ibb.co/r3SZq8S/footware.jpg"
-    }
-]
+let mongodb = require('mongodb').MongoClient;
+let url = process.env.mongoUrl
 
 function routing(routes){
 
